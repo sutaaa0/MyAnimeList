eeb0d585fdb9aca8b99fce8e5eeb572f2dd19ca4
dodi <dodisuta0@gmail.com>
2024-01-21 04:49:29 +0700

    fixed search feature

---
+++ src/app/animesearch/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CardTes from "../components/AnimeList/CardTes";
+
+export default async function SearchAnime ({api}) {
+  return (
+    <div className="grid  md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 p-3">
+      {api.data?.map((data) => {
+        return (
+          <div key={data.mal_id} className="shadow-xl ">
+            <CardTes title={data.title} episodes={data.episodes} type={data.type} images={data.images.webp.image_url} id={data.mal_id} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
--- src/app/search/[keyword]/page.js
+++ src/app/search/[keyword]/page.js
@@ -1,23 +1,19 @@
 "use client"
-
 import Headerlayout from "../../components/Header/HeaderLayout"
 import {getAnimeRespons} from "../../lib/getAnimeRespons"
-import TopAnime from "../../topanime/page"
+import SearchAnime from "../../animesearch/page"
 
 
 const Page = async ({params}) => {
   const {keyword} = params;
   const decode = decodeURI(keyword)
 
-  // const response = await fetch (`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decode}`)
-  // const searchAnime = await response.json()
-
  const searchAnime = await getAnimeRespons("anime", `q=${decode}`)
 
   return(
     <div className="w-full h-auto">
       <Headerlayout title={`Search For ${decode}`} />
-      <TopAnime api={searchAnime} />
+      <SearchAnime api={searchAnime} />
     </div>
   )
 }
