aeae3826f4e38af27099d1c7e15abf43f9af5c7a
dodi <dodisuta0@gmail.com>
2024-01-21 05:40:26 +0700

    fixed search feature

--- src/app/animesearch/page.jsx
+++ src/app/animesearch/page.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import CardTes from "../components/AnimeList/CardTes";
+import React from 'react'
+import Cardtes from "../components/AnimeList/CardTes"
 
-export default async function SearchAnime ({api}) {
-  return (
-    <div className="grid  md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 p-3">
-      {api.data?.map((data) => {
-        return (
-          <div key={data.mal_id} className="shadow-xl ">
-            <CardTes title={data.title} episodes={data.episodes} type={data.type} images={data.images.webp.image_url} id={data.mal_id} />
-          </div>
-        );
-      })}
-    </div>
-  );
+const SearchAnime = async ({ api }) => {
+
+    return (
+        <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
+            {api.data?.map((data, index) => {
+                return (
+                     <div key={index} className="shadow-xl ">
+                      <Cardtes title={data.title} episodes={data.episodes}
+                        type={data.type} images={data.images.webp.image_url}
+                        id={data.mal_id} />
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
+export default SearchAnime
--- src/app/search/[keyword]/page.js
+++ src/app/search/[keyword]/page.js
@@ -1,14 +1,13 @@
-"use client"
 import Headerlayout from "../../components/Header/HeaderLayout"
-import {getAnimeRespons} from "../../lib/getAnimeRespons"
+import { getAnimeRespons } from "../../lib/getAnimeRespons"
 import SearchAnime from "../../animesearch/page"
 
 
 const Page = async ({params}) => {
   const {keyword} = params;
-  const decode = decodeURI(keyword)
 
- const searchAnime = await getAnimeRespons("anime", `q=${decode}`)
+  const decode = decodeURI(keyword)
+  const searchAnime = await getAnimeRespons("anime", `q=${decode}`)
 
   return(
     <div className="w-full h-auto">
