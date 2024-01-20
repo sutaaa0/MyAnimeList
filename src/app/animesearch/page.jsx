7330726cf1fd10e2a720862958bbca390dd6c328
dodi <dodisuta0@gmail.com>
2024-01-21 05:46:06 +0700

    fixed search feature

--- src/app/animesearch/page.jsx
+++ src/app/animesearch/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Cardtes from "../components/AnimeList/CardTes"
 
-const SearchAnime = async ({ api }) => {
+const SearchAnime = async (api) => {
 
     return (
         <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
